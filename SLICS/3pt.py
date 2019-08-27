24778e4c45c8dc9d8ef79ffaa193b21760cc5127
Alex Malz <aimalz@nyu.edu>
2019-08-27 23:45:41 +0200

    coherent units

--- SLICS/3pt.py
+++ SLICS/3pt.py
@@ -16,4 +16,30 @@ with open(fn, 'rb') as f1:
 
 loc_data = pd.DataFrame(data=raw_data[2:], columns=['x', 'y', 'z', 'vx', 'vy', 'vz'])
 
+# number of MPI tasks per dimension
+nodes_dim = 4
+
+# subvolume size
+ncc = 768
+
+# volume size
+rnc = 3072.
+
+for k1 in range(1, nodes_dim+1):
+    for j1 in range(1, nodes_dim+1):
+        for i1 in range(1, nodes_dim+1):
+            if fn_index == (i1 - 1) + (j1 - 1) * nodes_dim + (k1 - 1) * nodes_dim ** 2:
+                print('found index '+str(fn_index)+' at '+str((i1, j1, k1)))
+                node_coords = {'x': i1 - 1, 'y': j1 - 1, 'z': k1 - 1}
+                print(node_coords)
+
+# shift data
+glob_data = loc_data
+for col in ['x', 'y', 'z']:
+    glob_data[col] = np.remainder(loc_data[col] + node_coords[col] * ncc, rnc)
+    assert(max(glob_data[col] <= rnc))
+
+# convert to Mpc/h
+phys_data = glob_data * 505. / 3072.
+
 loc_data.to_csv('for_treecorr.csv', header=False, index=False, sep=' ', columns=['x', 'y'])
