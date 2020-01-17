2a9762b139948a4286fb67e2d85dd097b7769703
Kara Ponder <kponder@berkeley.edu>
2020-01-17 15:32:46 -0700

    Updated limit on lowest redshift bin, updated distance names, pipeline runs per redshift, untested: updates to plots

--- GAMA/position_to_mock.py
+++ GAMA/position_to_mock.py
@@ -76,9 +76,12 @@ def redshift_bins(z, z_low=0.023, z_high=3.066):
         z_low: lower limit of lowest redshift bin. Default calculated for SLICS
         z_high: higher limit of highes redshift bin. Default calculated for SLICS
     """
-    z_mids = (z[1:] + z[:-1]) / 2.
-    z_bins = np.insert(z_mids, 0, z_low)
+
+    z_mids = (z[2:] + z[1:-1]) / 2.
+    z_bins = np.insert(z_mids, 0, 0.05949)
+    z_bins = np.insert(z_bins, 0, z_low)
     z_bins = np.append(z_bins, z_high)
+
     return z_bins
 
 def create_redshift_data(df, z, datadir='./', verbose=False, **kwargs):
@@ -154,11 +157,15 @@ def redshift_df(str_z, zenvdf, datadir='./'):
                                 'N_SPEC', 'N_GAMA_SPEC', 'DIST',
                                 'SPECID', 'SURVEY', 'SURVEY_CODE',
                                 'RA', 'DEC', 'WMIN', 'WMAX', 'Z', 'NQ',
-                                'PROB', 'FILENAME', 'URL', 'URL_IMG'])
+                                'PROB', 'FILENAME', 'URL', 'URL_IMG',
+                                'lsstg', 'lsstg_err', 'lsstr', 'lsstr_err',
+                                'lssti', 'lssti_err', 'lsstz', 'lsstz_err',
+                                'lssty', 'lssty_err', 'sdssr', 'sdssr_err',
+                                'sdssi', 'sdssi_err', 'sdssz', 'sdssz_err'])
     df = pd.merge(phodf, zenvdf, on=['CATAID'])
     return df
 
-def distance_bins(z, n=10, noz=False, verbose=False, **kwargs):
+def distance_bins(z, n=10, noz=False, verbose=False, **kwargs): ### NOTE: I REWROTE THIS
     """
         Returns list of angular distances.
 
@@ -187,7 +194,7 @@ def distance_bins(z, n=10, noz=False, verbose=False, **kwargs):
         ang_anchor = phys_anchors / da * 180. / np.pi
 
         try_distances = np.flip(np.geomspace(min(ang_anchor.value),
-                                             min(2.5, max(ang_anchor.value)),
+                                             min(2.0, max(ang_anchor.value)),
                                              n), axis=0)
 
     if verbose:
@@ -273,7 +280,7 @@ def run_clustering(str_z, zenvdf, n_clusters=10, metric="euclidean",
         info - may change waiting
     """
     try_distances = distance_bins(float(str_z))
-    str_tryd = [str(i) for i in try_distances]
+    str_tryd = [str(i) for i in np.arange(1, 11)]
 
     df = redshift_df(str_z, zenvdf)
     if float(str_z) < 0.3:
@@ -289,12 +296,12 @@ def run_clustering(str_z, zenvdf, n_clusters=10, metric="euclidean",
 
     df['label'] = km.labels_
 
-    if loat(str_z) < 0.3:
-        df['color1'] = df['lsstr']-df['lssti']
-        df['color2'] = df['lssti']-df['lsstz']
+    if float(str_z) < 0.3:
+        df['color1'] = df['lsstr'] - df['lssti']
+        df['color2'] = df['lssti'] - df['lsstz']
     else:
-        df['color1'] = df['lssti']-df['lsstz']
-        df['color2'] = df['lsstz']-df['lssty']
+        df['color1'] = df['lssti'] - df['lsstz']
+        df['color2'] = df['lsstz'] - df['lssty']
 
     if savefiles:
         os.makedirs('ts_kmeans', exist_ok=True)
@@ -422,7 +429,7 @@ def make_orchestra(z, zenvdf, savefig=True, verbose=False):
         df = redshift_df(str(z), zenvdf)
         if len(df) > 0:
             for i in range(len(orig_distances)):
-                parts = ax.violinplot(df[str(orig_distances[i])], positions=[i], vert=False)
+                parts = ax.violinplot(df[str(orig_distances[i])], positions=[i])
                 np.where(df[str(orig_distances[i])] < 1)
                 c = color[n]
                 for pc in parts['bodies']:
@@ -442,10 +449,10 @@ def make_orchestra(z, zenvdf, savefig=True, verbose=False):
             if verbose:
                 print("I have nothing for you at n=%s, z=%s"%(n,z))
 
-    plt.yticks(range(len(orig_distances)), np.around(orig_distances, 3))
-    ax.semilogx()
-    ax.set_ylabel('radial distance [deg]', size=15)
-    ax.set_xlabel('Normalized number of neighbors', size=15)
+    plt.xticks(range(len(orig_distances)), np.around(orig_distances, 3))
+    ax.semilogy()
+    ax.set_xlabel('radial distance [deg]', size=15)
+    ax.set_ylabel('Normalized number of neighbors', size=15)
 
     cax, _ = matplotlib.colorbar.make_axes(ax, pad=0.01)
     cbar = matplotlib.colorbar.ColorbarBase(cax, cmap=plt.cm.Spectral_r, norm=cNorm)
@@ -458,6 +465,43 @@ def make_orchestra(z, zenvdf, savefig=True, verbose=False):
     return
 
 
+def make_ang_phys_plot(savefig=True):
+    fig, ax = plt.subplots()
+    ang_dist_grid = []
+    for i in range(len(z_SLICS[0:8])):
+        try_distances = distance_bins(z, n=10)
+
+        dc = cosmo.comoving_distance(z_SLICS[i])
+        da = dc / (1 + z_SLICS[i])
+        ax.plot(np.flip(try_distances, axis=0) * da * np.pi / 180.,
+                np.flip(try_distances, axis=0), color=color[i], lw=3)
+
+    ax.semilogx()
+
+    # need to update this once we figure out which radii to use
+    ax.axvline(1, c='grey', alpha=0.4)
+    ax.axvline(2, c='grey', alpha=0.4)
+    ax.axvline(3, c='grey', alpha=0.4)
+    ax.axvline(5, c='grey', alpha=0.4)
+    ax.axvline(8, c='grey', alpha=0.4)
+    ax.axvline(20, c='grey', alpha=0.4)
+    ax.axvline(40, c='grey', alpha=0.4)
+    ax.axvline(60, c='grey', alpha=0.4)
+
+    ax.set_ylabel('Angular distances', fontsize=13)
+    ax.set_xlabel('Physical scales [Mpc]', fontsize=13)
+
+    plt.setp(ax, xticks=([1, 5, 10, 50, 70]), xticklabels=([1, 5, 10, 50, 70]))
+
+    cax, _ = matplotlib.colorbar.make_axes(ax, pad=0.01)
+    cbar = matplotlib.colorbar.ColorbarBase(cax, cmap=jet, norm=cNorm)
+    cbar.ax.set_ylabel('redshift', size=12)
+    if savefig:
+        plt.savefig('physical_scale_v_angular_distance.pdf')
+    return
+
+#### End Plotting routines
+
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
@@ -465,7 +509,10 @@ if __name__ == "__main__":
     parser.add_argument('--verbose', action='store_true', default=False)
     parser.add_argument('--no_files', dest='savefile',
                         default=True, action='store_false')
+    parser.add_argument('--outdir', default='./')
     parser.add_argument('--run_env', dest='run_environment',
+                        default=False, action='store_true')
+    parser.add_argument('--gen_summaries', dest='generate_fit_summaries',
                         default=False, action='store_true')
     parser.add_argument('--run_particle', dest='run_particle_environment',
                         default=False, action='store_true')
@@ -479,7 +526,7 @@ if __name__ == "__main__":
     create_redshift_data(df, z_SLICS)
 
     if opts.run_environment:
-
+        n = 10
         z_bins = redshift_bins(z_SLICS)
 
         RA_bin_ends = [0., 80., 160., 200., 360.]
@@ -505,7 +552,7 @@ if __name__ == "__main__":
         for f in range(len(subsamples)):
             (minx, maxx, miny, maxy) = field_bounds[f]
             for s in range(len(subsamples[f])) :
-                try_distances = distance_bins(z_SLICS[s])
+                try_distances = distance_bins(z_SLICS[s], n=n)
                 assert(max(try_distances) <= min((maxx - minx), (maxy - miny)) / 2.)
                 if opts.verbose:
                     print(lens[f][s])
@@ -518,37 +565,44 @@ if __name__ == "__main__":
                     nps = mp.cpu_count()
                     pool = mp.Pool(nps - 1)
                     envs_in_field = pool.map(calc_env, range(len(data)))
+                    if len(try_distances) < n:
+                        new_envs_in_field = []
+                        for envs in envs_in_field:
+                            envs += [''] * (n - len(try_distances))
+                            new_envs_in_field.append(envs)
+                        envs_in_field = new_envs_in_field
                     all_envs = all_envs + envs_in_field
                     pool.close()
 
         envs_arr = np.array(all_envs)
-        envs_df = pd.DataFrame(data=envs_arr, index = envs_arr[:, 0], columns = ['CATAID']+[str(i) for i in try_distances])
+        envs_df = pd.DataFrame(data=envs_arr, index = envs_arr[:, 0], columns = ['CATAID']+[str(i) for i in np.arange(1, n+1)])
 
-        zendf = pd.merge(envs_df, df, on='CATAID')
+        zenvdf = pd.merge(envs_df, df, on='CATAID')
 
         if opts.savefile:
-            path = os.path.join(outdir, 'enviros.csv')
-            df.to_csv(path)
+            path = os.path.join(opts.outdir, 'enviros.csv')
+            zenvdf.to_csv(path)
     else:
         try:
-            zenvdf = pd.read_csv('enviros.csv')
+            zenvdf = pd.read_csv('enviros.csv') # note: need specify location
         except FileNotFoundError:
             print('No enviros.csv file. Must run with --run_env flag to generate.')
 
-    #z_string = '0.08'
+    if opts.run_particle_environment:
+        particles = pd.read_csv('ang2deg.csv')
+
     for z_string in ['0.042', '0.080', '0.130', '0.221', '0.317', '0.418', '0.525', '0.640']:
-        df_w_label = run_clustering(z_string, zenvdf)
+        if opts.generate_fit_summaries:
+            df_w_label = run_clustering(z_string, zenvdf)
 
-        # create the fit summaries for every label
-        n_clusters = 10
-        label = np.arange(0, n_clusters, 1)
+            # create the fit summaries for every label
+            n_clusters = 10
+            label = np.arange(0, n_clusters, 1)
 
-        for l in label:
-            create_fit_summaries(df_w_label, l, z_string)
+            for l in label:
+                create_fit_summaries(df_w_label, l, z_string)
 
-        if run_particle_environment:
-            particles = pd.read_csv('ang2deg.csv')
-
+        if opts.run_particle_environment:
             try_distances = distance_bins(float(z_string))
 
             nrand = 3000 ## will be updated to function by Malz
@@ -572,15 +626,15 @@ if __name__ == "__main__":
                                    columns = ['CATAID']+[str(i) for i in try_distances])
 
             if opts.savefile:
-                path = os.path.join(outdir, 'particle_enviros.csv')
-                envs_df.to_csv(path)
+                path = os.path.join(opts.outdir, 'particle_enviros_%s.csv' % z_string)
+                envs_df.to_csv(path, index=False)
 
         else:
             try:
-                envs_in_field = pd.read_csv('particle_enviros.csv')
+                envs_df = pd.read_csv('particle_enviros_%s.csv' % z_string)
             except FileNotFoundError:
-                print('No particle_enviros.csv file.', \
+                print('No particle_enviros_%s.csv file.' % z_string, \
                       'Must run with --run_particle flag to generate.')
 
-        results = get_properties(envs_in_field[:].values[:,1:], str_red)
-        # ra, dec
+        # The results are a mock catalog
+        results = get_properties(envs_df[:].values[:,1:], z_string)
